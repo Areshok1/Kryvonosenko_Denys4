b35994ad850a3c39e21b5bcdb0a41c25bb73d063
Areshok1 <71553811+Areshok1@users.noreply.github.com>
2023-11-27 17:02:46 +0200

    Update PalindromeCheckerTest.java

--- src/test/java/org/example/PalindromeCheckerTest.java
+++ src/test/java/org/example/PalindromeCheckerTest.java
@@ -8,39 +8,28 @@ class PalindromeCheckerTest {
 
     @Test
     void isPalindrome_withPalindromeWord_shouldReturnTrue() {
-        // Arrange
         String palindromeWord = "level";
 
-        // Act
         boolean result = PalindromeChecker.isPalindrome(palindromeWord);
 
-        // Assert
         assertTrue(result);
     }
 
     @Test
     void isPalindrome_withNonPalindromeWord_shouldReturnFalse() {
-        // Arrange
         String nonPalindromeWord = "hello";
 
-        // Act
         boolean result = PalindromeChecker.isPalindrome(nonPalindromeWord);
 
-        // Assert
         assertFalse(result);
     }
 
     @Test
     void isPalindrome_withPalindromePhrase_shouldReturnTrue() {
-        // Arrange
         String palindromePhrase = "A man a plan a canal Panama";
 
-        // Act
         boolean result = PalindromeChecker.isPalindrome(palindromePhrase);
 
-        // Assert
         assertTrue(result);
     }
-
-    // Add more test cases as needed
 }
